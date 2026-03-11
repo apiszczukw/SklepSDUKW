3086bc312c9fe4d6ee4499eed0f489f43ae4a56d
apiszczukw <apiszczukw@gmail.com>
2026-03-11 09:23:03 +0100

    Stopka - strony statyczne; menu z kategoriami

--- SklepSDUKW/Program.cs
+++ SklepSDUKW/Program.cs
@@ -28,8 +28,16 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
+app.MapControllerRoute(
+    name: "Footer",
+    pattern: "Info/{name}",
+    defaults: new { controller = "Home", action = "FooterSites" }
+    );
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
