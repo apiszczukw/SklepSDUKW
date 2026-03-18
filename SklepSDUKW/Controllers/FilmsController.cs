0fe6be1c3c85ecc90b9b64d643714c2af5bbc7ee
apiszczukw <apiszczukw@gmail.com>
2026-03-18 09:26:57 +0100

    Komponent widoku menu + lista filmów z danej kategorii

---
+++ SklepSDUKW/Controllers/FilmsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SklepSDUKW.DAL;
+
+namespace SklepSDUKW.Controllers
+{
+    public class FilmsController : Controller
+    {
+        FilmsContext db;
+
+        public FilmsController(FilmsContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryFilms(string categoryName)
+        {
+            var category = db.Categories.Include("Films").Where(c => c.Name.ToUpper() == categoryName.ToUpper()).Single();
+
+            var films = category.Films.ToList();
+
+            return View(films);
+        }
+    }
+}
---
+++ SklepSDUKW/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SklepSDUKW.DAL;
+
+namespace SklepSDUKW.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        FilmsContext db;
+
+        public MenuViewComponent(FilmsContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View("_Menu", db.Categories.ToList()));
+        }
+    }
+}
